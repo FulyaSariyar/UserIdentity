34fc6085e34004beff8721f59f43dd6e11d8075d
Fulya Sarıyar Abdullah <fulya12agustos@hotmail.com>
2021-12-30 17:30:34 +0300

    Kullanıcı tanımlaması ok

--- UserIdentity/Controllers/AccountController.cs
+++ UserIdentity/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ using UserIdentity.Models;
 
 namespace UserIdentity.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
         private UserManager<ApplicationUser> userManager;
@@ -23,12 +25,54 @@ namespace UserIdentity.Controllers
 
             return View();
         }
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Login(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+
+            var user = userManager.Find(model.UserName, model.Password);
+            if (user == null)
+            {
+                ModelState.AddModelError("","Yanlış kullanıcı adı veya parola");
+            }
+            else
+            {
+                var authManager = HttpContext.GetOwinContext().Authentication;
+                var identity = userManager.CreateIdentity(user, "ApplicationCookie");
+                var authProperties = new AuthenticationProperties()
+                {
+                    IsPersistent = true
+
+                };
+                authManager.SignOut();
+                authManager.SignIn(authProperties, identity);
+
+                return Redirect(string.IsNullOrEmpty(returnUrl)? "/":returnUrl);
+            }
+            }
+            
+            ViewBag.returnUrl=returnUrl;
+            return View(model);
+        }
+
+        [AllowAnonymous]
         public ActionResult Register()
         {
             return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [AllowAnonymous]
         public ActionResult Register(Register model)
         {
 
@@ -53,6 +97,13 @@ namespace UserIdentity.Controllers
 
             }
             return View(model);
+        }
+        public ActionResult Logout()
+        {
+            var authManager = HttpContext.GetOwinContext().Authentication;
+            authManager.SignOut();
+
+            return RedirectToAction("login");
         }
     }
 }
